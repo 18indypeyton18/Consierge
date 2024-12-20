80da97ac200c4fe1375606e50eb5f3b1576b63af
Austin McLaughlin <austin.lake.mclaughlin@gmail.com>
2024-12-19 21:11:33 -0600

    Initial Commit

---
+++ Consierge/New Place/NewPlaceCellsCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  NewPlaceCellsCollectionViewCell.swift
+//  Consierge
+//
+//  Created by Austin McLaughlin on 12/11/24.
+//
+
+import UIKit
+
+class NewPlaceCellsCollectionViewCell: UICollectionViewCell {
+    
+}
